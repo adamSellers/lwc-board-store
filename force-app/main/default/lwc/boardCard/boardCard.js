2f4866806b77945a6c3a9112f9eb493d21535bab
Adam Sellers <adza007@gmail.com>
2019-02-25 17:18:08 -0700

    adding the board card

---
+++ force-app/main/default/lwc/boardCard/boardCard.js
@@ -0,0 +1,100 @@
+import {
+  LightningElement,
+  wire
+} from 'lwc';
+import {
+  CurrentPageReference,
+  NavigationMixin
+} from 'lightning/navigation';
+
+/* Wire adapter to load record detail */
+import {
+  getRecord
+} from 'lightning/uiRecordApi';
+
+/* pub/sub mechanism for sibling component comms */
+import {
+  registerListener,
+  unregisterAllListeners
+} from 'c/pubsub';
+
+/** Import the schema from Board__c as required */
+import BOARD_OBJECT from '@salesforce/schema/Board__c';
+import NAME_FIELD from '@salesforce/schema/Board__c.Name';
+import LEVEL_FIELD from '@salesforce/schema/Board__c.Ability_Level__c';
+import CAMBER_FIELD from '@salesforce/schema/Board__c.Camber__c';
+import SIZE_FIELD from '@salesforce/schema/Board__c.Size__c';
+import PRICE_FIELD from '@salesforce/schema/Board__c.Price__c';
+import IMAGE_NAME_FIELD from '@salesforce/schema/Board__c.Image_Name__c';
+import TYPE_FIELD from '@salesforce/schema/Board__c.Snowboard_Type__c';
+import DESCRIPTION_FIELD from '@salesforce/schema/Board__c.Description__c';
+import boardImages from '@salesforce/resourceUrl/boardImages';
+
+/** record fields to load.. */
+
+const fields = [
+  NAME_FIELD,
+  LEVEL_FIELD,
+  CAMBER_FIELD,
+  SIZE_FIELD,
+  PRICE_FIELD,
+  IMAGE_NAME_FIELD,
+  TYPE_FIELD,
+  DESCRIPTION_FIELD
+]
+
+/** Component to display the fields of Board__c */
+export default class BoardCard extends LightningElement {
+  /** Id of the board */
+  recordId;
+
+  /** image url to use for the component, loaded from the static resource
+   * boardImages. This will use the 200px image for the component.
+   */
+  boardImageUrl = boardImages + '/images/thumbs200/' + IMAGE_NAME_FIELD;
+
+  @wire(CurrentPageReference) pageRef;
+
+  /** load the board to display */
+  @wire(getRecord, {
+    recordId: '$recordId',
+    fields
+  })
+  board;
+
+  /** register the listener upon component load */
+  connectedCallback() {
+    registerListener('boardSelected', this.handleBoardSelected, this);
+  }
+
+  /** unregister the listener upon component destruction */
+  disconnectedCallback() {
+    unregisterAllListeners(this);
+  }
+
+  /** handler for when a board is selected. When the recordId changes, 
+   * the wire service above will update the data
+   */
+  handleBoardSelected(boardId) {
+    this.recordId = boardId;
+  }
+
+  /** handle the navigation event, a click will navigate to the SF record. 
+   * TODO - implement the add to cart functionality here instead of a nav
+   * event.
+   */
+  handleNavigateToRecord() {
+    this[NavigationMixin.Navigate]({
+      type: 'standard__recordPage',
+      attributes: {
+        recordId: this.recordId,
+        objectApiName: BOARD_OBJECT.objectApiName,
+        actionName: 'view'
+      }
+    });
+  }
+
+  get noData() {
+    return !this.board.error && !this.board.data;
+  }
+}
