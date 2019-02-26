b3d2c31d8a9f02260620b01dafc3ee97db5cc8d3
Adam Sellers <adza007@gmail.com>
2019-02-25 18:51:29 -0700

    board card works

--- force-app/main/default/lwc/boardCard/boardCard.js
+++ force-app/main/default/lwc/boardCard/boardCard.js
@@ -28,10 +28,11 @@ import PRICE_FIELD from '@salesforce/schema/Board__c.Price__c';
 import IMAGE_NAME_FIELD from '@salesforce/schema/Board__c.Image_Name__c';
 import TYPE_FIELD from '@salesforce/schema/Board__c.Snowboard_Type__c';
 import DESCRIPTION_FIELD from '@salesforce/schema/Board__c.Description__c';
-import boardImages from '@salesforce/resourceUrl/boardImages';
+
+/** static resource of product images */
+import BOARD_IMAGES from '@salesforce/resourceUrl/boardImages';
 
 /** record fields to load.. */
-
 const fields = [
   NAME_FIELD,
   LEVEL_FIELD,
@@ -48,11 +49,6 @@ export default class BoardCard extends LightningElement {
   /** Id of the board */
   recordId;
 
-  /** image url to use for the component, loaded from the static resource
-   * boardImages. This will use the 200px image for the component.
-   */
-  boardImageUrl = boardImages + '/images/thumbs200/' + IMAGE_NAME_FIELD;
-
   @wire(CurrentPageReference) pageRef;
 
   /** load the board to display */
@@ -65,6 +61,10 @@ export default class BoardCard extends LightningElement {
   /** register the listener upon component load */
   connectedCallback() {
     registerListener('boardSelected', this.handleBoardSelected, this);
+  }
+
+  renderedCallback() {
+    console.log('the name field object: ' + JSON.stringify(IMAGE_NAME_FIELD.fieldApiName));
   }
 
   /** unregister the listener upon component destruction */
@@ -96,5 +96,11 @@ export default class BoardCard extends LightningElement {
 
   get noData() {
     return !this.board.error && !this.board.data;
+  }
+
+  /** getter for the image url */
+  get boardImageUrl() {
+    let imgUrl = BOARD_IMAGES + '/images/thumbs200/' + this.board.data.fields.Image_Name__c.value;
+    return imgUrl;
   }
 }
