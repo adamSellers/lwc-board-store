7a71cd07e65ffe88fcfbbcf59706e99a8b45b0ba
Adam Sellers <adza007@gmail.com>
2019-03-02 12:19:25 -0700

    svg adding

---
+++ force-app/main/default/lwc/placeholder/placeholder.js
@@ -0,0 +1,14 @@
+import {
+  LightningElement,
+  api,
+  track
+} from 'lwc';
+
+import BOARD_IMAGES from '@salesforce/resourceUrl/boardImages';
+
+export default class Placeholder extends LightningElement {
+  @api message;
+
+  @track shredLogo = BOARD_IMAGES + '/shredHeadLogo.svg';
+
+}
