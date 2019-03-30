3f3f81287b410ea15dafa337ff9c8df2ff673baa
Adam Sellers <adza007@gmail.com>
2019-03-30 12:03:28 -0600

    in progress on billing address in AK

--- force-app/main/default/lwc/boardCartPayment/boardCartPayment.js
+++ force-app/main/default/lwc/boardCartPayment/boardCartPayment.js
@@ -1,8 +1,23 @@
 import {
-  LightningElement
+  LightningElement,
+  api
 } from 'lwc';
 
 export default class BoardCartPayment extends LightningElement {
+  /** shipping address details */
+  @api street;
+  @api city;
+  @api state;
+  @api zip;
+  @api country;
+
+  /** billing address fields */
+  _street;
+  _city;
+  _state;
+  _zip;
+  _country;
+  _useDefaultShippingDetails = false;
 
   handlePlaceOrder() {
     /** assuming payment confirmation is good! (TODO!!) */
@@ -11,5 +26,22 @@ export default class BoardCartPayment extends LightningElement {
 
   handleBackButton() {
     this.dispatchEvent(new CustomEvent('backbutton'));
+  }
+
+  handleUseShippingAddress() {
+    this._useDefaultShippingDetails = !this._useDefaultShippingDetails;
+    if (this._useDefaultShippingDetails) {
+      this._street = this.street;
+      this._city = this.city;
+      this._state = this.state;
+      this._zip = this.zip;
+      this._country = this.country;
+    } else {
+      this._street = '';
+      this._city = '';
+      this._state = '';
+      this._zip = '';
+      this._country = '';
+    }
   }
 }
